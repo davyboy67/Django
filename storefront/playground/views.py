d38d78704eb68389f7893330b39745ecac498e7b
davyboy67 <davidoduntan2@gmail.com>
2023-08-12 00:33:53 +0200

    Version 2.1

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from django.db.models import Q, F
+from django.db.models import Sum, Count, Avg, Min, Max
+from store.models import Product, OrderItem, Order
+from store.models import Customer
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', { 'name': 'Mosh' })
+    # keyword=value
+    #
+    # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    # queryset = Product.objects.filter(Q(unit_price__gt=70) & Q(unit_price__lt=90)).order_by('title')[15:30]
+    # queryset =  Product.objects.filter(id__in= OrderItem.objects.values('product_id').distinct()).order_by('title')
+    #queryset = Product.objects.select_related('collection').all() 
+    #queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    #select_related - other end of relationship has one instance
+    #prefetch_related - other end of relationship has many instances
+    result = Order.objects.aggregate(count=Count('id'))
+    result1 = OrderItem.objects.filter(product__id=1).aggregate(units_sold=Count('quantity'))
+    result2 = Order.objects.filter(customer__id=1).aggregate(orders_placed=Count('id'))
+    
+    
+    #queryset = Product.objects.values('id', 'title', 'orderitem__product_id').order_by('title') #inner join. 
+        
+    return render(request, 'hello.html', { 'name': 'Mosh', 'result': result, 'result1': result1, 'result2': result2, 'result3': result3  })
     
--- storefront/store/models.py
+++ storefront/store/models.py
@@ -39,11 +39,6 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
     
-    class Meta: # metadata
-        db_table = 'store_cutomers'
-        indexes = [
-            models.Index(fields=['last_name', 'first_name'])
-        ]
         
     
 class Order(models.Model):
